6f7f9e6c6568a85ce8a93af08c86562060ff5038
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-04-08 19:44:04 +0200

    Changed 'Part of walkner-av' to 'Part of the walkner-av project'.

--- client/app/main.js
+++ client/app/main.js
@@ -1,10 +1,6 @@
-/**
- * Copyright (c) 2014, Łukasz Walukiewicz <lukasz@walukiewicz.eu>. Some Rights Reserved.
- * Licensed under CC BY-NC-SA 4.0 <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
- * Part of walkner-av <http://lukasz.walukiewicz.eu/p/walkner-av>
- *
- * @preserve
- */
+// Copyright (c) 2014, Łukasz Walukiewicz <lukasz@walukiewicz.eu>. Some Rights Reserved.
+// Licensed under CC BY-NC-SA 4.0 <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
+// Part of the walkner-av project <http://lukasz.walukiewicz.eu/p/walkner-av>
 
 require.config({
   baseUrl: './',
